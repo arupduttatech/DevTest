f98ef437b7c43dcec99513db6f2b44d696c4b663
Arup <dutta.arup22@gmail.com>
2019-08-05 14:55:16 +0530

    Sample Interest Added

--- DevTest/src/Hello.java
+++ DevTest/src/Hello.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class Hello {
 
 	 public static void main(String args[]) 
-	    { 
-	        System.out.println("Hello, World"); 
-	
+	    {
+	        float p, r, t, sinterest;
+	        Scanner scan = new Scanner(System.in);
+	        System.out.print("Enter the Principal : ");
+	        p = scan.nextFloat();
+	        System.out.print("Enter the Rate of interest : ");
+	        r = scan.nextFloat();
+	        System.out.print("Enter the Time period : ");
+	        t = scan.nextFloat();
+	        scan.close();
+	        sinterest = (p * r * t) / 100;
+	        System.out.print("Simple Interest is: " +sinterest);
 }
 }
